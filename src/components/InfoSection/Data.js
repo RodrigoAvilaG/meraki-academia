a97605827290251ca5a03a2696e6a15d6fdf3e42
Rodrigo Avila <rodrigoavilag12@gmail.com>
2021-07-20 23:27:02 -0500

    New cooking class added (info section)

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Proxima clase",
+  headLine: "Curso de cocina vegan, carnitas de setas estilo Michoacán",
+  description:
+    "En este curso aprenderás a cocinar unas carnitas con un sabor y un sazón espectacular y sus dos salsas tradicionales para carnitas usando como ingrediente principal la setas un hongo con una versatilidad y un sabor para chuparse los dedos en la cocina vegana.",
+  buttonLabel: "Comprar",
+  imgStart: false,
+  img: require("../../images/svg-1.svg"),
+  alt: "Food",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
